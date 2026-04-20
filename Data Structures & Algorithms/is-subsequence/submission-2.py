69829f06aff3486a7044c5a46fee33e18fa1eaf8
Simon Risk <76711152+SimonNRisk@users.noreply.github.com>
2026-04-20 12:37:43 -0400

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        t_index = 0
+        for letter in s:
+            while t_index < len(t) and t[t_index] != letter:
+                t_index +=1
+            if t_index == len(t):
+                return False
+            t_index +=1
+        return True
