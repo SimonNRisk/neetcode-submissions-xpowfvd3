4aa743ecd32248f495e66f5a90c7ebdd93d0e66f
Simon Risk <76711152+SimonNRisk@users.noreply.github.com>
2026-06-13 17:53:26 -0400

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # binary search
+        # 
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left + right) // 2
+            print(mid)
+            if target > nums[mid]:
+                left = mid + 1
+            elif target < nums[mid]:
+                right = mid - 1
+            else: ## target == nums[mid]
+                return mid  
+        return right + 1   
+        if nums[mid] > target:
+            return mid
+        else:
+            return mid + 1
+
+        # always insert where it will be bigger than to its left
+        # 
