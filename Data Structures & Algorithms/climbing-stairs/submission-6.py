dd9a17bdb22760e2da8a27730d471853423623d6
Simon Risk <76711152+SimonNRisk@users.noreply.github.com>
2026-06-14 17:38:05 -0400

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,27 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return  1
+        if n==2:
+            return 2
+        # we only care about the last 2 numbers
+        prev = 1
+        cur = 2
+        for _ in range(2, n):
+            prev, cur = cur, cur+prev
+
+        return cur
+
+
+
+        
+        
+        '''
+        cache = {1:1, 2:2}
+        def f(x):
+            if x in cache:
+                return cache[x]
+            cache[x] = f(x-1) + f(x-2)
+            return cache[x]
+        return f(n)
+        '''
