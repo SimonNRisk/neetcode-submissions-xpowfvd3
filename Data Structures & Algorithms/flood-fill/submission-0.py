5486f1e54da352943f79b04f5810eafff6255cd1
Simon Risk <76711152+SimonNRisk@users.noreply.github.com>
2026-08-26 12:59:48 -0400

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        num_rows = len(image)
+        num_cols = len(image[0])
+        color_to_change = image[sr][sc]
+        target_color = color
+        seen = set()
+
+        def dfs(r, c):
+            nonlocal color_to_change
+            nonlocal target_color
+            nonlocal seen
+            if image[r][c] != color_to_change:
+                return
+            if (r, c) in seen:
+                return
+            image[r][c] = target_color
+            seen.add((r, c))
+            # call on neighbours
+            # Up
+            if r > 0:
+                dfs(r-1, c)
+            # Down
+            if r < num_rows-1:
+                dfs(r+1, c)
+            # Left
+            if c > 0:
+                dfs(r, c-1)
+            # Right
+            if c < num_cols -1:
+                dfs(r, c+1)
+            return
+        dfs(sr, sc)
+        return image
+
+            
