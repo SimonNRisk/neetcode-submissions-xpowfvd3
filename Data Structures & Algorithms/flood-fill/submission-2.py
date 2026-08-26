fbfeb94119c1fc0977dcdeed248a20d1bca9e041
Simon Risk <76711152+SimonNRisk@users.noreply.github.com>
2026-08-26 18:23:13 -0400

    Add: flood-fill - submission-2

---
+++ Data Structures & Algorithms/flood-fill/submission-2.py
@@ -0,0 +1,64 @@
+from collections import deque
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        num_rows = len(image)
+        num_cols = len(image[0])
+        if image[sr][sc] == color:
+            return image
+        q = deque([(sr, sc)])
+        color_to_change = image[sr][sc]
+        while q:
+            (r, c) = q.popleft()
+            image[r][c] = color
+            
+            # BFS
+            # Up
+            if r > 0 and image[r-1][c] == color_to_change:
+                q.append((r-1, c))
+            # Down
+            if r < num_rows-1 and image[r+1][c] == color_to_change:
+                q.append((r+1, c))
+            # Left
+            if c > 0 and image[r][c-1] == color_to_change:
+                q.append((r, c-1))
+            # Right
+            if c < num_cols-1 and image[r][c+1] == color_to_change:
+                q.append((r, c+1))
+        return image
+        '''
+        color_to_change = image[sr][sc]
+        target_color = color
+        seen = set()
+        if target_color == color_to_change:
+            return image
+
+        def dfs(r, c):
+            nonlocal color_to_change
+            nonlocal target_color
+            nonlocal seen
+            if image[r][c] != color_to_change:
+                return
+            if (r, c) in seen:
+                return
+            image[r][c] = target_color
+            seen.add((r, c))
+            # call on neighbours
+            # Up
+            if r > 0:
+                dfs(r-1, c)
+            # Down
+            if r < num_rows-1:
+                dfs(r+1, c)
+            # Left
+            if c > 0:
+                dfs(r, c-1)
+            # Right
+            if c < num_cols -1:
+                dfs(r, c+1)
+            return
+        dfs(sr, sc)
+        return image
+        '''
+
+            
